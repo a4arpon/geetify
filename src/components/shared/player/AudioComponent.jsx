3f0a3f408f3c75440275102914687a053792626a
Mr Wayne <a4arpon@gmail.com>
2023-05-30 01:19:43 +0600

    Core: Mainfest Added

--- src/components/shared/player/AudioComponent.jsx
+++ src/components/shared/player/AudioComponent.jsx
@@ -1,10 +1,12 @@
 import useTitle from '../../../hooks/useTitle'
 const AudioComponent = ({ playerPacket }) => {
-  const { audioPlayer, getCurrentDuration, setDuration } = playerPacket
   useTitle('Ben 10 Guitar Remix | GeetiFy')
+  const audio =
+    'https://ve33.aadika.xyz/download/V1Pl8CzNzCw/mp3/320/1685345397/ac7478073a9718f62e22797ca5d83d32d75ed6d0b869094d58e12ddeb80e6d0c/1?f=X2Download.app'
+  const { audioPlayer, getCurrentDuration, setDuration } = playerPacket
   return (
     <audio
-      src="/audio/runway.mp3"
+      src={audio}
       ref={audioPlayer}
       onTimeUpdate={getCurrentDuration}
       onLoadedData={(a) => {
