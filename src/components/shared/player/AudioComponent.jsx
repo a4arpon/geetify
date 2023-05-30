39c2a1bff7fd4d01f3eac9b1bfba3906f0437d6d
Mr Wayne <a4arpon@gmail.com>
2023-05-30 12:40:18 +0600

    Fix: Env

--- src/components/shared/player/AudioComponent.jsx
+++ src/components/shared/player/AudioComponent.jsx
@@ -2,6 +2,7 @@ import useTitle from '../../../hooks/useTitle'
 const AudioComponent = ({ playerPacket }) => {
   useTitle('Ben 10 Guitar Remix | GeetiFy')
   const audio = 'https://s12.gmpdn.com/get/3984754/Ad9J9w4XO9h2nb0pDYRZIqR51w51e4CQxMMmZNlp/mp3'
+  // const audio = '/audio/runway.mp3'
   const { audioPlayer, getCurrentDuration, setDuration } = playerPacket
   return (
     <audio
