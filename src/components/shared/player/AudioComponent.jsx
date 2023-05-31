eef3c7b62d819a453fd60eff60ffca62df1a0f30
Mr Wayne <a4arpon@gmail.com>
2023-05-31 12:58:24 +0600

    Fix: Audio Player Fixed

--- src/components/shared/player/AudioComponent.jsx
+++ src/components/shared/player/AudioComponent.jsx
@@ -1,8 +1,9 @@
 import useTitle from '../../../hooks/useTitle'
 const AudioComponent = ({ playerPacket }) => {
   useTitle('Ben 10 Guitar Remix | GeetiFy')
-  const audio = 'https://s12.gmpdn.com/get/3984754/Ad9J9w4XO9h2nb0pDYRZIqR51w51e4CQxMMmZNlp/mp3'
-  // const audio = '/audio/runway.mp3'
+  // https://dl50.yt2mp3.icu/m4a/QX8vJ8GQ5jg::91daa8dbcf57eb24fb21cb53b147b70c::1685433637::5be693b4::d
+  // https://ve122.aadika.xyz/download/KeXrYNrMB_g/mp3/128/1685437791/0952386f8374b71659d211cb3567814dee1e72c79f72419c763f350f642e4843/1?f=yt5s.io
+  const audio = '/audio/runway.mp3'
   const { audioPlayer, getCurrentDuration, setDuration } = playerPacket
   return (
     <audio
--- src/pages/home/only_covers/Covers.jsx
+++ src/pages/home/only_covers/Covers.jsx
@@ -1,4 +1,3 @@
-import { Parallax } from 'react-parallax'
 import bgImg1 from '../../../assets/backgrounds/1.webp'
 import bgImg2 from '../../../assets/backgrounds/2.webp'
 import bgImg3 from '../../../assets/backgrounds/3.webp'
@@ -6,18 +5,39 @@ import bgImg4 from '../../../assets/backgrounds/4.webp'
 import bgImg5 from '../../../assets/backgrounds/5.webp'
 import bgImg6 from '../../../assets/backgrounds/6.webp'
 const Covers = () => {
-  const bgArr = [bgImg1, bgImg2, bgImg5, bgImg6, bgImg4, bgImg3]
+  const bgArr = [
+    {
+      id: 1,
+      img: bgImg1,
+      title: 'Mid Night Chills',
+      totalFollowers: 1200232,
+    },
+    {
+      id: 2,
+      img: bgImg2,
+      title: 'Midnight Lofi Vibes ',
+      totalFollowers: 232323,
+    },
+    { id: 3, img: bgImg5, title: 'Asthetic Vibs', totalFollowers: 2334343 },
+    { id: 4, img: bgImg6, title: 'Bold Life', totalFollowers: 2232 },
+    {
+      id: 5,
+      img: bgImg4,
+      title: 'You and me. Love each other.',
+      totalFollowers: 232323,
+    },
+    { id: 6, img: bgImg3, title: 'Rules Are Broken', totalFollowers: 23433 },
+  ]
   return (
     <div>
-      {bgArr.map((bgImg, index) => (
-        <Parallax
-          key={index}
-          blur={{ min: -15, max: 15 }}
-          bgImage={bgImg}
-          strength={-200}
+      {bgArr.map((content) => (
+        <div
+          key={content.id}
+          className="h-[calc(100vh+50vh)] bg-cover"
+          style={{ backgroundImage: `url(${content.img})` }}
         >
-          <div className="h-screen"></div>
-        </Parallax>
+          <div className="h-full w-full bg-base-100 bg-opacity-50"></div>
+        </div>
       ))}
     </div>
   )
