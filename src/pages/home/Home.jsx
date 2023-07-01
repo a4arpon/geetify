dcd02733387783b120d62d65fa7542d01429dbed
Mr Wayne <a4arpon@gmail.com>
2023-07-02 00:34:27 +0600

    fix: private fix added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,16 @@
+import { useDispatch } from 'react-redux'
+import { audioActions } from '../../states/AudioSlice'
+
 const Home = () => {
+  const dispatch = useDispatch()
+  const { setAudio } = audioActions
+  const tryState = () => {
+    dispatch(setAudio('Hi'))
+  }
   return (
-    <div>
+    <div className="py-20">
       <h1>Hello World</h1>
+      <button onClick={tryState}>Fuck Me</button>
     </div>
   )
 }
