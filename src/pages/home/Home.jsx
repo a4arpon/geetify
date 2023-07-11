54dae6a0229e798942c5face72cb43ea6d704165
Mr Wayne <a4arpon@gmail.com>
2023-07-11 18:18:34 +0600

    Map error test

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,18 +2,22 @@ import axios from 'axios'
 import { useEffect, useState } from 'react'
 import AudioItem from '../../components/shared/audioItem/AudioItem'
 const Home = () => {
-  const [audios, setAudios] = useState({})
+  const [audios, setAudios] = useState([])
   useEffect(() => {
     axios
       .get(`${import.meta.env.VITE_SERVER_URL}audios/all`)
       .then((data) => data?.data)
       .then((res) => setAudios(res))
+
+    return () => {
+      setAudios([])
+    }
   }, [])
 
   return (
     <div className="grid lg:grid-cols-4 gap-2">
       {audios?.map((item) => (
-        <AudioItem key={item?.thumbnail} item={item} />
+        <AudioItem key={item?.thumbnail} item={item}/>
       ))}
     </div>
   )
