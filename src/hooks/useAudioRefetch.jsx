fd49dd411cecd609c1fc8a1dce963ca8401dd3a4
Mr Wayne <a4arpon@gmail.com>
2023-08-03 20:23:52 +0600

    error fixed

--- src/hooks/useAudioRefetch.jsx
+++ src/hooks/useAudioRefetch.jsx
@@ -2,11 +2,12 @@ import axios from 'axios'
 import { useEffect, useState } from 'react'
 import useAudio from './useAudio'
 
-const useAudioRefetch = (error) => {
+const useAudioRefetch = () => {
   const { audio } = useAudio()
   const [refreshedAudio, setRefreshedAudio] = useState(audio)
   useEffect(() => {
-    if (audio?.refreshLink && error) {
+    if (audio?.refreshLink && audio?.isError) {
+      console.log('fetching...')
       axios
         .post('https://geetify-web-sr.a4arpon.repl.co/audioRefresh', {
           refreshLink: audio?.refreshLink,
@@ -19,12 +20,13 @@ const useAudioRefetch = (error) => {
             mediaUrl: res?.download_url,
           }
           setRefreshedAudio({ ...audio, mediaUrl: mediaPacket?.mediaUrl })
+          console.log(refreshedAudio)
         })
         .catch((err) => console.log(err))
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [audio?.refreshLink])
-  if (error) {
+  if (audio?.isError && audio?.mediaUrl !== refreshedAudio?.mediaUrl) {
     console.log('Path from', refreshedAudio)
     return refreshedAudio
   }
